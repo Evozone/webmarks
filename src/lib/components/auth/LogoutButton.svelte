0b1c7e62d0ae54d7850bc1bdea8fba5ecec68c6f
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-05-22 02:56:11 +0530

    fix: Make logout button smaller

--- src/lib/components/auth/LogoutButton.svelte
+++ src/lib/components/auth/LogoutButton.svelte
@@ -1,34 +1,31 @@
 <script>
     // Imports
-    import { navigate } from "svelte-routing";
-    import { auth } from "../../../firebase";
+    import { navigate } from 'svelte-routing';
+    import { auth } from '../../../firebase';
 
     // Stores
-    import { loggedInUser, showLoading } from "../../../stores";
+    import { loggedInUser, showLoading } from '../../../stores';
 
     // Icons
-    import Icon from "svelte-icons-pack/Icon.svelte";
-    import FiLogOut from "svelte-icons-pack/fi/FiLogOut";
+    import Icon from 'svelte-icons-pack/Icon.svelte';
+    import FiLogOut from 'svelte-icons-pack/fi/FiLogOut';
 
     // This function logs out the user
     const logout = async () => {
         $showLoading = true;
         try {
             await auth.signOut();
-            await localStorage.removeItem("webmarksToken");
+            await localStorage.removeItem('webmarksToken');
             loggedInUser.set(null);
-            navigate("/");
+            navigate('/');
             $showLoading = false;
         } catch (error) {
-            console.error("Error while logging out:", error);
+            console.error('Error while logging out:', error);
         }
     };
 </script>
 
-<button
-    on:click={logout}
-    class="btn btn-primary flex items-center justify-between"
->
+<button on:click={logout} class="btn btn-primary btn-sm flex items-center justify-between">
     <span>Logout</span>
     <Icon src={FiLogOut} />
 </button>
